960ec900337d73fa5e4ceca2c50d2af4ae481e45
Gali Ravi Praveen <grpnpraveen@gmail.com>
2024-08-06 18:13:29 -0400

    before memoji

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ function App() {
       </div>
 
 
-      <Memoji/>
+      {/* <Memoji/> */}
       {/* <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
       
       <a className="relative block px-3 py-2 transition text-teal-500 dark:text-teal-400" href="/about">About<span class="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0"></span></a>
