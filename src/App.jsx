48a08dfe38bbecb1fb8c0ab2596e1f358d2a81e9
Gali Ravi Praveen <grpnpraveen@gmail.com>
2024-08-21 18:06:06 -0400

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import Loadingsvg from '/loading.svg';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
@@ -8,16 +8,35 @@ import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
 import Eca from './components/Eca';
+import { motion } from "framer-motion";
 // import { AnimatePresence } from 'framer-motion';
 
 function App() {
-  const [show_image, set_show_image] = useState(false);
+  const [show_image, set_show_image] = useState(true);
   const location = useLocation();
+
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.5, // Delay between each sentence
+      },
+    },
+  };
+  
+  const sentenceVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  };
+
+
+
 
   useEffect(() => {
     const timer = setTimeout(() => {
       set_show_image(false);
-    }, 3000);
+    }, 4000);
 
     return () => clearTimeout(timer); // Clean up the timer
   }, []);
@@ -27,11 +46,39 @@ function App() {
       <div className='flex flex-col items-center justify-center self-center place-content-center'>
         {show_image ? (
           <>
-            <img src={Loadingsvg} alt="Loading..." />
+                   <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    <img src={Loadingsvg} alt="Loading..." className='ml-10'/>
+                    <motion.div
+                      className="flex flex-row space-x-2 text-center pt-10 loading_text justify-center"
+                      variants={containerVariants}
+                      initial="hidden"
+                      animate="visible"
+                    >
+                      <motion.p variants={sentenceVariants}>L</motion.p>
+                      <motion.p variants={sentenceVariants}>o</motion.p>
+                      <motion.p variants={sentenceVariants}>a</motion.p>
+                      <motion.p variants={sentenceVariants}>d</motion.p>
+                      <motion.p variants={sentenceVariants}>i</motion.p>
+                      <motion.p variants={sentenceVariants}>n</motion.p>
+                      <motion.p variants={sentenceVariants}>g</motion.p>
+                      <motion.p variants={sentenceVariants}>.</motion.p>
+                      <motion.p variants={sentenceVariants}>.</motion.p>
+                      <motion.p variants={sentenceVariants}>.</motion.p>
+                      <motion.p variants={sentenceVariants}>.</motion.p>
+                      <motion.p variants={sentenceVariants}>.</motion.p>
+                    </motion.div>
+                    {/* <div className='flex flex-col text-center pt-10 loading_text'>
+                      <p>L o a d i n g ....</p>
+                    </div> */}
+                  </div>
           </>
         ) : (
           <>
-            {/* <AnimatePresence> */}
+          <Suspense fallback={
+             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+             <img src={Loadingsvg} alt="Loading..." />
+           </div>
+          }>
             <Routes location={location} key={location.pathname}>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
@@ -40,7 +87,7 @@ function App() {
               <Route path="/skills" element={<Skills />} />
               <Route path="/eca" element={<Eca />} />
             </Routes>
-            {/* </AnimatePresence> */}
+            </Suspense>
           </>
         )}
       </div>
