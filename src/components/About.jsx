ccb84efc071890bf9b4d2aec9ce69903aa2ce089
Gali Ravi Praveen <grpnpraveen@gmail.com>
2024-08-27 18:55:09 -0400

    Till 2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ function About()
 
       
         {/* ONE */}
-       <div className="flex flex-col mt-16 pl-16 text-xl text-[#cecac3]">
+       <div className="flex flex-col mt-16 pl-20 text-xl text-[#cecac3]">
 
         <motion.div
           initial={{ x: '-200%' }}
@@ -22,50 +22,27 @@ function About()
 
           <div className="text-left w-[60%]">
             Hi, I'm <span className=" text-orange-500">Ravi Praveen Gali</span> (He/Him), currently pursuing a Master's in Computer Science at Stony Brook University, NY. My journey in the tech world is driven by a deep passion for programming. 
-            {/* <br></br> */}
+            <br></br>
+            <br></br>
             With a strong foundation in Python, AI/ML, and Data Science, I specialize in building comprehensive solutions from the ground up. My journey in tech is rooted in continuous learning, and I've honed my skills in both front-end and back-end development. 
+            <br></br>
+            <br></br>
+            My enthusiasm for technology extends to hackathons, where I have consistently excelled. I am proud to have emerged as a winner in the prestigious Smart India Hackathon, a national-level competition. Additionally, I have ranked in the top 3 in nearly six other hackathons. 
+
           </div> 
-        </motion.div>
+     
 
 
-        {/* Two */}
-        <motion.div
-          initial={{ x: '-200%' }}
-          animate={{ x: '0%' }}
-          exit={{ x: '100%' }}
-          transition={{ duration: 3, ease: [0.22, 1, 0.36, 1] }}
-        >
-        <div className="flex flex-row mt-28">
-
-          <div className="size-[40%] bg-black">
-
-
-
-          </div>
-
-
-          <div className="text-left text-xl w-[60%] pr-16">
-          My enthusiasm for technology extends to hackathons, where I have consistently excelled. 
-          <br></br> 
-          I am proud to have emerged as a winner in the prestigious Smart India Hackathon, a national-level competition. Additionally, I have ranked in the top 3 in nearly six other hackathons. 
-          </div>
-
-        </div> 
         
-        </motion.div>
 
       {/* Three */}
       
-      <motion.div
-          initial={{ x: '-200%' }}
-          animate={{ x: '0%' }}
-          exit={{ x: '100%' }}
-          transition={{ duration: 3, ease: [0.22, 1, 0.36, 1] }}
-        >
-      <div className="flex flex-row mt-28">
+      <div className="flex flex-row mt-14">
 
       <div className="text-left text-xl w-[60%]">
       These experiences have not only sharpened my technical skills but also fostered a mindset of creative problem-solving, and in my free time, I enjoy creating 3D models and spending time in Blender.
+      {/* <br></br> */}
+      As I continue my academic journey at Stony Brook University, I look forward to further deepening my knowledge in computer science. I am excited about the opportunities to collaborate with brilliant minds and contribute to cutting-edge research in the field.
       </div>
 
 
@@ -77,62 +54,25 @@ function About()
 
       </div> 
 
-</motion.div>
 
 
 
 
 
 
-      
 
-
-      {/* Four */}
-      <motion.div
-          initial={{ x: '-200%' }}
-          animate={{ x: '0%' }}
-          exit={{ x: '100%' }}
-          transition={{ duration: 3, ease: [0.22, 1, 0.36, 1] }}
-        >
-
-      <div className="flex flex-row mt-28">
-
-        <div className="size-[40%] bg-black">
-
-
-
-        </div>
-
-
-        <div className="text-left text-xl w-[60%] pr-16">
-        As I continue my academic journey at Stony Brook University, I look forward to further deepening my knowledge in computer science. I am excited about the opportunities to collaborate with brilliant minds and contribute to cutting-edge research in the field.
-        </div>
-
-        </div> 
-
-
-    </motion.div>
 
         {/* Five */}
 
-        <motion.div
-          initial={{ x: '-200%' }}
-          animate={{ x: '0%' }}
-          exit={{ x: '100%' }}
-          transition={{ duration: 3, ease: [0.22, 1, 0.36, 1] }}
-        >
+
         <div className="flex flex-row mt-28">
 
         <div className="text-left text-xl w-[60%]">
-        Let's connect and explore the exciting possibilities at the intersection of technology. Feel free to reach out—I'm always up for a meaningful conversation or collaboration! 🤝
+        Let's connect and explore the exciting possibilities at the intersection of technology. Feel free to reach out - I'm always up for a meaningful conversation or collaboration! 🤝
         </div>
 
 
-          <div className="size-[40%] bg-black">
-
-            
-
-          </div>
+          <div className="size-[40%] bg-black"></div>
 
         </div> 
 
@@ -160,10 +100,10 @@ function About()
 
 
 
-        <div className="flex flex-col items-center justify-center self-center mt-28">
+        <div className="flex flex-col self-start mt-28">
           <a href={"mailto:grpnpraveen@gmail.com"} target='blank'>
             <div className="flex flex-col rounded-[10px] bg-orange-600 text-2xl">
-              <div className="flex flex-row self-center text-center font-bold text-black p-2">
+              <div className="flex flex-row font-bold text-black p-2">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-10">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
                 </svg>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,18 +4,56 @@ import Timeline from "./Timeline";
 function Experience()
 {
  
+  const color_to = "text-yellow-500"
+  
     return(
       <>
       <Transition text_color_to='text-yellow-500' bg_color_to='bg-yellow-500' heading='Experience' star_color_to="rgb(234, 179, 8,">
 
 
-         <div className="flex flex-col pl-24">
+         <div className="flex flex-col pl-20">
 
-        <Timeline time="March 90" title="First ONe" info="Some thing 3" ></Timeline>
-        <Timeline time="March 30" title="First ONe" info="some thing two" href="https://www.google.com" ></Timeline>
-        <Timeline time="March 90" title="First ONe" info="Some thing 3" ></Timeline>
-        <Timeline time="March 90" title="First ONe" info="Some thing 3" ></Timeline>
-        <Timeline time="March 30" title="First ONe" info="some thing two" href="https://www.google.com" ></Timeline>
+        <Timeline time="Aug 2023 - Dec 2023" title="Graduate Teaching Assistant" subhead="Stony Brook University, Department of Computer Science" info="• Under the guidance of Dr. Kevin McDonnell, facilitated student proficiency in computer science with Python-based weekly lab
+        sessions, hands-on exercises, and homework support. Assessed assignments and midterms for a class with 30 students." color_to={color_to}></Timeline>
+       
+       
+        <Timeline time="Oct 2022 - Apr 2023" title="Software Engineering Intern" subhead="Yubi (Unicorn Startup), Supply Chain Finance Team"  
+          info="•Played a key role in the Integrations Team within the Supply Chain Finance team as a backend developer, specializing in
+                  seamlessly integrating 4 different client APIs with SCF's API using Ruby on Rails, Postgres, AWS, and Redis.
+                •Increased productivity with Forest Admin Automation, achieving 3x faster results. Streamlined manual ticket processes and
+                client data management, leveraging the flexibility of integrating Ruby on Rails with Forest Admin.
+                •Ensured security and data integrity by implementing robust encryption and decryption functionalities for 10 bank API calls,
+                significantly enhancing the overall security measures of the system.
+                •Utilized Redis to streamline and optimize database calls, achieving a 7% improvement in overall system efficiency and a 5%
+                enhancement in response times.
+                •Completed 300 plus tickets in six months, positioning the SCF team as a top performer in innovative solution delivery, by actively
+                contributing to the development of Supply Chain Finance logic. Additionally, integrated 2 new banks into SCF." 
+          color_to={color_to} ></Timeline>
+       
+        <Timeline time="Apr 2022 - Oct 2022" title="Backend Engineer Intern" subhead="Sabudh Foundation (Remote), Depression Detection Online"  
+          info=" •Developed an online Depression Detection platform using Python, Flask framework, Flask Blueprints library, Postgres, and
+ Psycopg2 library.
+ •Designed 20 database tables, including schema for authentication, authorization, patient details, hospitals, doctors, and
+ appointments, ensuring a robust foundation for the project.
+ •Orchestrated the creation of a user API tailored for doctors, patients, administrators, and other user types, ensuring a customized
+ experience for over 3 user categories.
+ •Streamlined and integrated a machine learning algorithm into the backend, revolutionizing the platform’s diagnostic capabilities
+ by analyzing patient videos for signs of depression, resulting in more than 90% accuracy and enhanced treatment outcomes." 
+          color_to={color_to} ></Timeline>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
